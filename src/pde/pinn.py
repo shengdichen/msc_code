03280c5be36b82bc265ee835592d22a5ed9c3722
shc <sh@c>
2024-01-08 19:14:33 +0100

    chore(mishra-pinn-demo): deeper cleanup

--- src/pde/pinn.py
+++ src/pde/pinn.py
@@ -3,9 +3,8 @@ import logging
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
-import torch.nn as nn
 
-from src.pde.multidiff import Multidiff
+from src.pde.multidiff import MultidiffNetwork
 from src.pde.pde import Distance
 
 logger = logging.getLogger(__name__)
@@ -26,70 +25,49 @@ class SolverExact:
             torch.exp(-self._delta * lhs) * 2 * capital_a * torch.cos(phi + omega * lhs)
         )
 
-    def eval_boundary_time(self, lhs: torch.Tensor) -> torch.Tensor:
+    def u_d0(self) -> float:
         return 1
 
+    def u_d1(self) -> float:
+        return 0
 
-class NetworkDense(nn.Module):
+
+class NetworkDense(torch.nn.Module):
     def __init__(
         self,
         size_hidden: int,
-        n_layers: int,
-        activation=nn.Tanh,
+        n_hiddens: int,
+        activation=torch.nn.Tanh,
     ):
         super().__init__()
 
-        self._size_layer_hidden = size_hidden
-        self._n_layers_hidden = n_layers
+        self._size_hidden, self._n_hiddens = size_hidden, n_hiddens
         self._activation = activation
 
-        self._layer_start = nn.Sequential(nn.Linear(1, size_hidden), activation())
-        # hiddens = []
-        # for _ in range(self._n_layers_hidden - 1):
-        #     hiddens.append(
-        #         nn.Sequential(
-        #             nn.Linear(self._size_layer_hidden, self._size_layer_hidden),
-        #             activation(),
-        #         )
-        #     )
-        #     # hiddens.append(nn.Linear(self._size_layer_hidden, self._size_layer_hidden))
-        #     # hiddens.append(self._activation())
-        # self._layers_hidden = nn.Sequential(*hiddens)
+        self._layers = self._make_layers()
 
-        self._layers_hidden = nn.Sequential(
-            *[
-                nn.Sequential(nn.Linear(size_hidden, size_hidden), activation())
-                for _ in range(n_layers - 1)
-            ]
-        )
-        self._layer_end = nn.Linear(size_hidden, 1)
-
-        # self._layers = self._make_layers()
-
-    def _make_layers(self) -> nn.Module:
+    def _make_layers(self) -> torch.nn.Module:
         layers = []
 
-        layers.append(nn.Linear(1, self._size_layer_hidden))
+        layers.append(torch.nn.Linear(1, self._size_hidden))
+        for _ in range(self._n_hiddens):
+            layers.append(torch.nn.Linear(self._size_hidden, self._size_hidden))
+        layers.append(torch.nn.Linear(self._size_hidden, 1))
 
-        layers.append(self._activation())
-        for _ in range(self._n_layers_hidden):
-            layers.append(nn.Linear(self._size_layer_hidden, self._size_layer_hidden))
-            layers.append(self._activation())
-
-        layers.append(nn.Linear(self._size_layer_hidden, 1))
-
-        return nn.Sequential(*layers)
+        return torch.nn.ModuleList(layers)
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
-        return self._layer_end(self._layers_hidden(self._layer_start(input)))
-        # return self._layers(input)
+        res = self._activation()(self._layers[0](input))
+        for layer in self._layers[1:-1]:
+            res = self._activation()(layer(res))
+        return self._layers[-1](res)
 
 
 class Pinn:
     def __init__(self):
         torch.manual_seed(123)
-        self._network = NetworkDense(32, 3)
-        print(self._network)
+        self._network = NetworkDense(32, 2)
+        logger.info(f"network\n{self._network}")
 
         self._optimiser = torch.optim.Adam(self._network.parameters(), lr=1e-3)
         self._loss_lambda_1, self._loss_lambda_2 = 1e-1, 1e-4
@@ -104,10 +82,10 @@ class Pinn:
 
         constant_delta, constant_omega_0 = 2, 20
         self._constant_mu, self._constant_k = 2 * constant_delta, constant_omega_0**2
-        self._t_test = torch.linspace(0, 1, 300).reshape(-1, 1)
 
+        self._gridpts_test = torch.linspace(0, 1, 300).reshape(-1, 1)
         self._solver_exact = SolverExact(constant_delta, constant_omega_0)
-        self._u_exact = self._solver_exact.eval_pde(self._t_test)
+        self._u_exact = self._solver_exact.eval_pde(self._gridpts_test)
 
     def train(self) -> None:
         for timestep in range(15001):
@@ -118,47 +96,44 @@ class Pinn:
 
         loss_d0, loss_d1 = self._loss_boundary_time()
         loss_pde = self._loss_pde()
+        loss = loss_d0 + self._loss_lambda_1 * loss_d1 + self._loss_lambda_2 * loss_pde
 
-        if timestep % 100 == 0:
+        loss.backward()
+        self._optimiser.step()
+
+        if timestep % 500 == 0:
             msg = (
-                f"step: {timestep}"
+                f"step: {timestep:05}"
                 " | "
-                f"loss-d0: {loss_d0}"
-                " | "
-                f"loss-d1: {loss_d1}"
-                " | "
-                f"loss-pde: {loss_pde}"
+                f"loss: {loss:.4}"
+                " [(d0, d1, pd) = "
+                f"({loss_d0:5.4}, {loss_d1:5.4}, {loss_pde:5.4})"
+                "]"
             )
             logger.info(msg)
             if timestep % 5000 == 0:
                 logger.info("plotting")
                 self._plot_progress(timestep)
 
-        loss = loss_d0 + self._loss_lambda_1 * loss_d1 + self._loss_lambda_2 * loss_pde
-        loss.backward()
-        self._optimiser.step()
-
     def _loss_boundary_time(self) -> tuple[torch.Tensor, torch.Tensor]:
-        u_d0 = self._network(self._gridpts_boundary)  # (1, 1)
-        loss_d0 = Distance(u_d0, 1).mse()
+        mdn = MultidiffNetwork(self._network, self._gridpts_boundary, order=1)
 
-        md = Multidiff(rhs=u_d0, lhs=self._gridpts_boundary)
-        u_dt1 = md.diff()
-        loss_d1 = Distance(u_dt1, 0).mse()
-
-        return loss_d0, loss_d1
+        return (
+            Distance(mdn.diff(0), self._solver_exact.u_d0()).mse(),
+            Distance(mdn.diff(1), self._solver_exact.u_d1()).mse(),
+        )
 
     def _loss_pde(self) -> torch.Tensor:
-        u_d0 = self._network(self._gridpts_pde)  # (30, 1)
-        md = Multidiff(rhs=torch.sum(u_d0), lhs=self._gridpts_pde)
-        u_d1t = md.diff()
-        u_d2t = md.diff()
-
-        pred = u_d2t + self._constant_mu * u_d1t + self._constant_k * u_d0
-        return Distance(pred, 0).mse()
+        mdn = MultidiffNetwork(self._network, self._gridpts_pde, order=2)
+        network_pde = (
+            self._constant_k * mdn.diff(0)
+            + self._constant_mu * mdn.diff(1)
+            + mdn.diff(2)
+        )
+        return Distance(network_pde, 0).mse()
 
     def _plot_progress(self, timestep: int) -> None:
-        prediction = self._network(self._t_test).detach()
+        prediction = self._network(self._gridpts_test).detach()
 
         plt.figure(figsize=(6, 2.5))
         plt.scatter(
@@ -178,14 +153,14 @@ class Pinn:
             alpha=0.6,
         )
         plt.plot(
-            self._t_test[:, 0],
+            self._gridpts_test[:, 0],
             self._u_exact[:, 0],
             label="Exact solution",
             color="tab:grey",
             alpha=0.6,
         )
         plt.plot(
-            self._t_test[:, 0],
+            self._gridpts_test[:, 0],
             prediction[:, 0],
             label="PINN solution",
             color="tab:green",
