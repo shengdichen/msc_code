b1eb85082bb134f166392e9e707d7f4e09dc3410
shc <sh@c>
2024-03-09 11:48:21 +0100

    feat(dataset): extract dataset split/mask interface

--- src/pde/dataset.py
+++ src/pde/dataset.py
@@ -1,9 +1,11 @@
 import abc
 import typing
 
+import numpy as np
 import torch
 
 from src.numerics import grid
+from src.util import dataset
 
 
 class DatasetPDE:
@@ -28,4 +30,51 @@ class DatasetPDE2d(DatasetPDE):
 
     @abc.abstractmethod
     def solve_instance(self) -> typing.Iterable[torch.Tensor]:
+        raise NotImplementedError
+
+
+class DatasetSplits:
+    def __init__(self, dataset_full: torch.utils.data.dataset.TensorDataset):
+        self._dataset_full = dataset_full
+        self._n_instances = len(self._dataset_full)
+
+    def split(
+        self, n_instances_eval: int, n_instances_train: int
+    ) -> tuple[
+        torch.utils.data.dataset.TensorDataset, torch.utils.data.dataset.TensorDataset
+    ]:
+        indexes_eval, indexes_train = self._indexes_eval_train(
+            n_instances_eval, n_instances_train
+        )
+
+        return (
+            torch.utils.data.Subset(self._dataset_full, indexes_eval),
+            torch.utils.data.Subset(self._dataset_full, indexes_train),
+        )
+
+    def _indexes_eval_train(
+        self, n_instances_eval: int = 300, n_instances_train: int = 100
+    ) -> tuple[np.ndarray, np.ndarray]:
+        # NOTE:
+        # generate indexes in one call with |replace| set to |False| to guarantee strict
+        # separation of train and eval datasets
+        indexes = np.random.default_rng(seed=42).choice(
+            self._n_instances,
+            n_instances_eval + n_instances_train,
+            replace=False,
+        )
+        return indexes[:n_instances_eval], indexes[-n_instances_train:]
+
+
+class DatasetMasked:
+    def __init__(
+        self,
+        dataset_raw: torch.utils.data.dataset.TensorDataset,
+        masks: typing.Sequence[dataset.Masker],
+    ):
+        self._dataset_raw = dataset_raw
+        self._masks = masks
+
+    @abc.abstractmethod
+    def make(self) -> torch.utils.data.dataset.TensorDataset:
         raise NotImplementedError
