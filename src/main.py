a5c43cfaab0bd05e0f82253bd31b2dae8ed1b0dd
shc <sh@c>
2024-05-12 11:14:24 +0200

    feat(main): log with time

--- src/main.py
+++ src/main.py
@@ -28,6 +28,8 @@ def main():
 
 if __name__ == "__main__":
     logging.basicConfig(
-        format="%(module)s: [%(levelname)s] %(message)s", level=logging.INFO
+        format="[%(asctime)s] %(module)s: [%(levelname)s] %(message)s",
+        level=logging.INFO,
+        datefmt="%Y.%m.%d-%H:%M:%S",
     )
     main()
